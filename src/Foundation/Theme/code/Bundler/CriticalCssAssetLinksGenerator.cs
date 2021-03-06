7eabdb35895b85dfd556642f0d60e70ff74154fe
vsedletskyi <vsedletskiy@gmail.com>
2021-03-06 19:00:27 +0200

    Add CriticalCssAssetLinkGenerator class which will resposible for generation styles and scripts which are marked as critical in our theme item

---
+++ src/Foundation/Theme/code/Bundler/CriticalCssAssetLinksGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.XA.Foundation.Theming.Bundler;
+using Sitecore.XA.Foundation.Theming.EventHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.Theme.Bundler
+{
+    public class CriticalCssAssetLinksGenerator : OptimizedAssetLinksGenerator
+    {
+        public CriticalCssAssetLinksGenerator()
+            : base()
+        {
+        }
+
+        //As usual we should use this static method to re-use in layout cshtml file
+        public new static AssetLinks GenerateLinks(IThemesProvider themesProvider)
+        {
+            if (AssetContentRefresher.IsPublishing() || CriticalCssAssetLinksGenerator.IsAddingRendering())
+            {
+                return new AssetLinks();
+            }
+
+            return new CriticalCssAssetLinksGenerator().GenerateAssetLinks(themesProvider);
+        }
+
+    }
+}
