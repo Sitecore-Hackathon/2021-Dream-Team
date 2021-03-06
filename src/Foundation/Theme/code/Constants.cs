20e37606e6d82c1aed57582b37725d10538a98b2
vsedletskyi <vsedletskiy@gmail.com>
2021-03-06 19:18:12 +0200

    Add media request handler for generation critical css optimized item

---
+++ src/Foundation/Theme/code/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Foundation.Theme
+{
+    public static class Constants
+    {
+        public const string ConcatenatedAndMinifiedFilename = "criticalcss-optimized-min.css";
+        public const string ConcatenatedFilename = "criticalcss-optimized.css";
+    }
+}
---
+++ src/Foundation/Theme/code/Pipelines/MediaRequestHandler/CriticalCssFiles.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Resources.Media;
+using Sitecore.XA.Foundation.MediaRequestHandler.Pipelines.MediaRequestHandler;
+using System.Linq;
+
+namespace Foundation.Theme.Pipelines.MediaRequestHandler
+{
+    public class CriticalCssFiles : Sitecore.XA.Foundation.Theming.Pipelines.MediaRequestHandler.OptimizedFiles
+    {
+        public override void Process(MediaRequestHandlerArgs handlerArgs)
+        {
+            if (handlerArgs.Media != null)
+            {
+                return;
+            }
+
+            var path = handlerArgs.Context.Request.Path;
+            var request = MediaManager.ParseMediaRequest(handlerArgs.Context.Request);
+
+            //Only this part was modified from standard base class
+            var source = new string[]
+                {
+                    Constants.ConcatenatedAndMinifiedFilename,
+                    Constants.ConcatenatedFilename
+                };
+
+            if (!source.Any(path.Contains))
+            {
+                return;
+            }
+
+            var item = this.ReGenerateOptimizedItem(path, request);
+            if (item != null)
+            {
+                handlerArgs.Media = MediaManager.GetMedia(item);
+            }
+        }
+    }
+}
