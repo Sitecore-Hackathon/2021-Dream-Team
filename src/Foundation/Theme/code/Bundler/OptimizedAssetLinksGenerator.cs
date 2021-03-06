6415b848db5b3f07776b5e040cad406c54d585a9
vsedletskyi <vsedletskiy@gmail.com>
2021-03-06 18:41:04 +0200

    Add code which are resposible for loading specific styles and scripts  for CM and CD

--- src/Foundation/Theme/code/Bundler/OptimizedAssetLinksGenerator.cs
+++ src/Foundation/Theme/code/Bundler/OptimizedAssetLinksGenerator.cs
@@ -1,12 +1,20 @@
 ﻿using Sitecore.Configuration;
+using Sitecore.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Diagnostics;
 using Sitecore.Pipelines;
 using Sitecore.SecurityModel.License;
+using Sitecore.XA.Foundation.Theming;
 using Sitecore.XA.Foundation.Theming.Bundler;
 using Sitecore.XA.Foundation.Theming.Configuration;
 using Sitecore.XA.Foundation.Theming.EventHandlers;
 using Sitecore.XA.Foundation.Theming.Pipelines.AssetService;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.Theming.Extensions;
+using Sitecore.Data;
 
 namespace Foundation.Theme.Bundler
 {
@@ -81,6 +89,41 @@ namespace Foundation.Theme.Bundler
             {
                 this.AddPlainInclude(assetInclude as PlainInclude, assetLinks);
             }
+        }
+
+        protected override void AddThemeInclude(ThemeInclude themeInclude, AssetLinks result, IThemesProvider themesProvider)
+        {
+            var themeItem = themeInclude.Theme;
+            if (themeItem == null && !themeInclude.ThemeId.IsNull)
+            {
+                themeItem = this.ContentRepository.GetItem(themeInclude.ThemeId);
+            }
+
+            if (themeItem == null)
+            {
+                return;
+            }
+
+            Log.Debug($"Starting optimized files generation process for {themeItem.Name} with following configuration {this._configuration}");
+
+            var themingContext = ServiceLocator.ServiceProvider.GetService<IThemingContext>();
+            var allThemes = themingContext.GetAllThemes(themeItem);
+
+            IList<Item> selectedThemes;
+            
+            //Above part was reused from standard AssetLinksGenerator. Bellow part checking if this is Exp Editor or not, base on this info, use different field for loading base themes
+            if (!Sitecore.Context.PageMode.IsExperienceEditor)
+            {
+                var allThemesForContentDelivery = themingContext.GetThemesWithBaseThemes(themeItem, new List<ID>(), Templates._ThemeSettings.Fields.LoadSelectedThemesOnContentDelivery);
+                selectedThemes = themesProvider.GetThemes(themeItem, allThemesForContentDelivery).Union((allThemesForContentDelivery.FilterBaseThemes() ?? new List<Item>())).ToList();
+            }
+            else
+            {
+                selectedThemes = (allThemes.FilterBaseThemes() ?? new List<Item>()).Union(themesProvider.GetThemes(themeItem, allThemes)).ToList();
+            }
+
+            this.GetLinks(allThemes.FilterBaseThemes(), this._configuration.ScriptsMode, this._configuration.StylesMode, result);
+            this.GetLinks(themesProvider.GetThemes(themeItem, allThemes), this._configuration.ScriptsMode, this._configuration.StylesMode, result);
         }
     }
 }
--- src/Foundation/Theme/code/Templates.cs
+++ src/Foundation/Theme/code/Templates.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Sitecore.Data;
 
 namespace Foundation.Theme
 {
   public struct Templates
   {
-  }
+        public struct _ThemeSettings
+        {
+            public static readonly ID TemplateId = new ID("{8E4ACCF3-E2EF-43EC-A34B-B81474676911}");
+
+            public struct Fields
+            {
+                public static readonly ID LoadSelectedThemesOnContentDelivery = new ID("{E4E1919D-5296-4527-A341-8C87EE6D9453}");
+            }
+        }
+    }
 }
